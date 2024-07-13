f246494e693df0c00f58b6bb67df962a16254b43
stashymane <aestheticrice@gmail.com>
2024-07-13 08:28:16 +0300

    make devicemanager actually work

--- model/src/commonMain/kotlin/model/device/DeviceManager.kt
+++ model/src/commonMain/kotlin/model/device/DeviceManager.kt
@@ -3,17 +3,13 @@ package model.device
 import dev.stashy.midifunk.device.MidiDevice
 
 object DeviceManager {
-    private val aliases = fetchAliases()
-    private val metaCache: MutableMap<String, DeviceMeta> = mutableMapOf()
+    val mappings: Map<String, DeviceMeta> =
+        getDatabase().flatMap { entry -> entry.names.map { name -> name to entry } }.toMap()
 
     fun list(): List<MidilinkDevice> = MidiDevice.list().map { device ->
-        val meta = aliases[device.name]?.let { getMeta(it) } ?: DeviceMeta()
+        val meta = mappings[device.name] ?: DeviceMeta()
         MidilinkDevice(device, meta)
     }
-
-    private fun getMeta(id: String) =
-        metaCache.getOrPut(id) { fetchMeta(id) ?: DeviceMeta() }
 }
 
-expect fun fetchAliases(): Map<String, String>
-expect fun fetchMeta(deviceId: String): DeviceMeta?
+expect fun getDatabase(): List<DeviceMeta>
--- model/src/jvmMain/kotlin/model/device/DeviceManager.jvm.kt
+++ model/src/jvmMain/kotlin/model/device/DeviceManager.jvm.kt
@@ -2,11 +2,20 @@ package model.device
 
 import kotlinx.serialization.json.Json
 
-actual fun fetchAliases(): Map<String, String> =
-    loadResource("device-aliases.json")?.let(Json.Default::decodeFromString) ?: mapOf()
+actual fun getDatabase(): List<DeviceMeta> = meta
 
-actual fun fetchMeta(deviceId: String): DeviceMeta? =
-    loadResource("devices/$deviceId.json")?.let { Json.Default.decodeFromString<DeviceMeta>(it) }
+val meta: List<DeviceMeta> by lazy {
+    devices.mapNotNull { loadResource("/devices/$it.json") }.map(Json.Default::decodeFromString)
+}
 
 private fun loadResource(path: String): String? =
-    DeviceManager::class.java.getResourceAsStream(path)?.readAllBytes()?.decodeToString()
+    DeviceManager::class.java.getResource(path)?.readText()
+
+private val devices =
+    listOf(
+        "gervill",
+        "komplete-audio-6-mk2",
+        "ms-midi-mapper",
+        "ms-wavetable-synth",
+        "real-time-sequencer"
+    ) //this is actually awful and i want to get rid of this soon
